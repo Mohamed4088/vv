a8e3bdb85e344d3bb0da986fa9c7d89eedc3b7e1
Mohamed4088 <130283427+Mohamed4088@users.noreply.github.com>
2026-02-05 14:04:54 +0400

    Update fetch_vless.py

--- fetch_vless.py
+++ fetch_vless.py
@@ -1,11 +1,10 @@
-import requests
+from urllib.request import urlopen
 
 URL = "https://raw.githubusercontent.com/roosterkid/openproxylist/refs/heads/main/V2RAY_RAW.txt"
 
-r = requests.get(URL, timeout=30)
-r.raise_for_status()
+data = urlopen(URL, timeout=30).read().decode("utf-8")
 
-vless = [l for l in r.text.splitlines() if l.startswith("vless://")]
+vless = [l for l in data.splitlines() if l.startswith("vless://")]
 
 with open("vless.txt", "w", encoding="utf-8") as f:
     f.write("\n".join(vless))
